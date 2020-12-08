f435cb0de5f5bf592910d07f9722257867ed45d0
kjempire9 <kjempirecode@gmail.com>
2020-12-08 13:50:03 +0300

    added favicons and a navbar

--- music.py
+++ music.py
@@ -1,4 +1,9 @@
-from flask import Flask
+'''
+The required docstring
+'''
+
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -6,5 +11,5 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('base.html', title='Romoz')
