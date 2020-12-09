1520488c079560f336fbc73991e374d2b29ad8ce
kjempire9 <kjempirecode@gmail.com>
2020-12-09 18:55:33 +0300

    added basic routes in my app file

--- music.py
+++ music.py
@@ -13,3 +13,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('base.html', title='Romoz')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html', title='Admin')
+
+@app.route('/videos')
+def video():
+    return render_template('videos.html', title='Videos')
+
+@app.route('/audios')
+def audio():
+    return render_template('audios.html', title='Audios')
+
