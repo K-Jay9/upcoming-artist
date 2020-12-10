5ecf1a247fbddd228cb5ec179df6ac85be2db34c
kjempire9 <kjempirecode@gmail.com>
2020-12-10 19:10:57 +0300

    added sqlite as the db to the backend

--- music.py
+++ music.py
@@ -24,12 +24,19 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+def get_data():
+    con = sqlite3.connect(DATABASE)
+    db = con.cursor()
+    res = db.execute('select * from links')
+    return res
+
 # closing the db connection after use
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 #  Initialising the db schema
 def init_db():
     with app.app_context():
@@ -45,8 +52,6 @@ def initdb_command():
     init_db()
     print('Initialised the database...')
 
-
-
 '''
 The actual app
 '''
@@ -54,59 +59,65 @@ The actual app
 # The home route of the project
 @app.route('/')
 def index():
-    res = get_db()
+    res = get_data()
     aud = []
     vid = []
-    # for i in res:
-    #     out = video_or_audio(i)
-    #     if out == YOUTUBE and out != None:
-    #         vid.append(a)
-    #     elif out == MIXCLOUD and out != None:
-    #         aud.append(a)
-    return render_template('index.html', title='Romoz', res=res)
+    for i in res:
+        out = video_or_audio(i[1])
+        if out == 'video' and out != None:
+            vid.append(i[1])
+        elif out == 'audio' and out != None:
+            aud.append(i[1])
+    return render_template('index.html', title='Romoz', aud=aud, vid=vid)
 
-@app.route('/admin/add')
+@app.route('/admin/add', methods=['POST', 'GET'])
 def add():
     msg= 'nthing'
-    try:
-        link = request.form['link']
-        with sql.connect(DATABASE) as con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO links link VALUES ? ",link )
-            
-            con.commit()
-            msg = "Record successfully added"
-    except:
-        con.rollback()
-        msg = "error in insert operation"
-    
-    finally:
-        return render_template("admin.html",msg = msg, title='Admin')
-        close_connection()
+    if request.method == 'POST':
+        try:
+            link = request.form['link']
+            with sql.connect(DATABASE) as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO links link VALUES ? ",link )
+                
+                con.commit()
+                msg = "Record successfully added"
+                print(msg)
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+        
+        finally:
+            return render_template("add.html",msg = msg, title='Add')
+            close_connection()
+    else:
+        print(msg)
+        return render_template('add.html', msg=msg, title='Add')
+
 @app.route('/admin')
 def admin():
-    rows = get_db()
+    rows = get_data()
     return render_template("admin.html",rows=rows,title='Admin')
 
 
 @app.route('/videos')
 def video():
-    res = get_db()
+    res = get_data()
     videos = []
-    # for i in res:
-    #     out = video_or_audio(i)
-    #     if out == YOUTUBE:
-    #         videos.append(out)
+    for i in res:
+        out = video_or_audio(i[1])
+        if out == 'video':
+            videos.append(i[1])
     return render_template('videos.html', title='Videos', videos=videos)
 
 @app.route('/audios')
 def audio():
-    res = get_db()
+    res = get_data()
     audios = []
-    # for i in res:
-    #     out = video_or_audio(i)
-    #     if out == MIXCLOUD and out != None:
-    #         audios.append(out)
+    for i in res:
+        out = video_or_audio(i[1])
+        if out == 'audio' and out != None:
+            audios.append(i[1])
     return render_template('audios.html', title='Audios', audios=audios)
 
 
@@ -114,7 +125,7 @@ def audio():
 
 def video_or_audio(d):
     if d[:len(YOUTUBE)] == YOUTUBE:
-        return d[len(YOUTUBE):]
+        return 'video'
     elif d[:len(MIXCLOUD)] == MIXCLOUD:
-        return d[len(MIXCLOUD):]
+        return 'audio'
     return None
